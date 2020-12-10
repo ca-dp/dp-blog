0c7c03925a389edad96ac2d6aca823b02a63917a
wasabeef <dadadada.chop@gmail.com>
2020-12-11 06:50:08 +0900

    chore(*): shuffle members

--- src/components/ScrollableMembers.tsx
+++ src/components/ScrollableMembers.tsx
@@ -1,11 +1,22 @@
 import Link from "next/link";
-import { members } from "@members";
-import { getMemberPath } from "@src/utils/helper";
+import {members} from "@members";
+import {getMemberPath} from "@src/utils/helper";
+import {Member} from "@src/types";
+
+function shuffle<T>(array: T[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
 
 export const ScrollableMembers: React.FC = () => {
   return (
     <div className="scrollable-members">
-      {members.map((member, i) => (
+      {shuffle<Member>(members).map((member, i) => (
         <Link
           key={`scrollable-member-${i}`}
           href={getMemberPath(member.name)}
