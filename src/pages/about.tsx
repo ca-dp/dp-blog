a7d847cbf6ea351e5129e8d78be961ddc5217126
wasabeef <dadadada.chop@gmail.com>
2020-12-11 19:20:40 +0900

    chore(*): update about text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,7 +12,7 @@ const Page: NextPage = () => {
           <h1 className="about__title">About</h1>
           <div className="about__body">
             <p>
-              このブログは株式会社サイバーエージェントの Developer Productivity team に 所属するメンバーの投稿を一覧にまとめて表示します。
+              このブログは株式会社サイバーエージェントの Developer Productivity team に 所属するメンバーのブログを一覧にまとめて表示します。
             </p>
           </div>
           <div className="about__actions">
